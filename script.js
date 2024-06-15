00a2944cddce4217b59a854b0ea5e035c9860508
Rafee <156030423+rafeer192@users.noreply.github.com>
2024-06-14 19:25:51 -0700

    Change resolution of canvas on button click

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
+const CANVAS_SIZE = 804; 
 const gridContainer = document.querySelector(".grid-container"); 
+
+function allowDraw() {
+  const boxes = document.querySelectorAll(".grid-container div"); 
+  boxes.forEach((box) => box.addEventListener("mouseenter", enterHandler)); 
+
+  function enterHandler() { 
+    this.classList.add("hovered");
+  }
+}
+
+// create starting 16x16 grid
 for(let i = 0; i < 16; ++i) {
   for(let j = 0; j < 16; ++j) {
     const box = document.createElement("div"); 
+    box.classList.add("default"); 
     gridContainer.appendChild(box); 
   }
 }
-const boxes = document.querySelectorAll(".grid-container div"); 
-boxes.forEach((box) => box.addEventListener("mouseenter", enterHandler)); 
+allowDraw(); 
 
-function enterHandler() {
-  this.classList.add("hovered");
-}
-
-
+const changeBtn = document.querySelector("button.change-res"); 
+changeBtn.addEventListener("click", () => {
+  let newResolution = Number(prompt("Please enter the new number of pixels per row (max. 100)", "16")); 
+  if(newResolution > 100 || newResolution < 1) {
+    alert("Resolution cannot be greater than 100 or less than 1!"); 
+  }
+  else {
+    while(gridContainer.firstChild) {
+      gridContainer.removeChild(gridContainer.lastChild); 
+    }
+    const boxSize = Math.floor(CANVAS_SIZE / newResolution); 
+    for(let i = 0; i < newResolution; ++i) {
+      const newStartBox = document.createElement("div"); 
+      for(let j = 0; j < newResolution; ++j) {
+        const newBox = document.createElement("div"); 
+        newBox.style.cssText = `height: ${boxSize}px; width: ${boxSize}px; border: 1px solid black; margin: 0px; padding: 0px;`; 
+        newStartBox.appendChild(newBox); 
+      }
+      gridContainer.appendChild(newStartBox); 
+    }
+    allowDraw(); 
+  }
+});  
+// NOTE 6/14: TAKING A BREAK, BUT COME BACK AND CONSIDER CHANGING THE WAY YOU CREATE GRIDS, HAVE A STARTING BLOCK WITH NO STYLE AND THEN
+// APPEND THE DIVS OF A COLUMN TO THAT STARTING DIV AND THEN APPEND THAT ENTIRE DIV TO GRIDCONTAINER
+// ALSO USE BETTER VARIABLE NAMES
 
 window.addEventListener("beforeunload", (event) => {
   event.preventDefault(); 
